daefb30ce915adae23f9865a2050d022ab39b408
David Popescu <david.popescu@6s.rocks>
2021-04-09 22:56:30 +0300

    configuration done

---
+++ back-end/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const config = require('./config/app')
+
+const app = express()
+
+app.get('/home', (req, res) => {
+    return res.send("Home Sweet Home");
+});
+
+app.get('/boss', (req, res) => {
+    return res.send("Home Sweet Bome");
+});
+const port = process.env.APP_PORT;
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`); 
+});
+
+console.log("dic ");
